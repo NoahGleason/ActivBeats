3de50df2af0c34322087d688bb338a52c1539c3d
Noah Gleason <noah.gleason1@gmail.com>
2019-10-26 19:06:53 -0400

    Finish implementing Sample

--- app/src/main/java/com/example/bluetooth/Sample.kt
+++ app/src/main/java/com/example/bluetooth/Sample.kt
@@ -2,15 +2,16 @@ package com.example.bluetooth
 
 import com.example.bluetooth.wav.WavFile
 
-class Sample(private val peak: Int, private val duration: Double, private val start: Double, private val data : DoubleArray) {
-
+class Sample(private val peak: Int, private val duration: Double, private val start: Double, private val data : DoubleArray, private val naturalDuration: Double) {
 
     class SampleFactory(wav: WavFile) {
 
         private var data : DoubleArray? = null
+        private val naturalDuration : Double
 
         init {
             val arrayListData = WavFile.getRaw(wav, 0)
+            naturalDuration = arrayListData.size.toDouble() / wav.sampleRate
             data = DoubleArray(arrayListData.size)
             for (i in arrayListData.indices){
                 data!![i] = arrayListData[i]
@@ -18,12 +19,18 @@ class Sample(private val peak: Int, private val duration: Double, private val st
         }
 
         fun getSample(peak : Int, duration: Long, start: Long) : Sample {
-            return Sample(peak, duration.toDouble() / 1000., start.toDouble() / 1000., data!!)
+            return Sample(peak, duration.toDouble() / 1000.0, start.toDouble() / 1000.0, data!!, naturalDuration)
         }
     }
 
+    private val ratio = data.size / duration
+
     fun getValueAtTime(t : Double) : Double{
-        if (t < start)
+        if (t < start || t > start + duration){
+            return 0.0
+        }
+        val index = (t - start) * ratio
+        return data[index.toInt()] + (data[kotlin.math.ceil(index).toInt()] - data[index.toInt()]) * (index - index.toInt())
     }
 
 }
